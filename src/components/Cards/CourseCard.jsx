818f0474fba565998ea26c65166d58b560d67b0c
difmo <difmotech@gmail.com>
2024-11-09 14:51:07 +0530

    save

--- src/components/Cards/CourseCard.jsx
+++ src/components/Cards/CourseCard.jsx
@@ -1,79 +1,48 @@
 import React from "react";
-import icon1 from "../../assets/images/about.png";
-
-const mydata = [
-  {
-    images: icon1,
-    title: "Master Data Structures and Algorithms using C++ ",
-  },
-];
-import {
-  FaGlobe,
-  FaLaptopCode,
-  FaAward,
-  FaGraduationCap,
-} from "react-icons/fa";
+import { FaGlobe, FaLaptopCode, FaAward, FaGraduationCap } from "react-icons/fa";
 import { BsFillPersonFill } from "react-icons/bs";
 
-const CourseCard = () => {
+const CourseCard = ({ course }) => {
   return (
-    <div className="py-4 ">
-      <div className="overflow-hidden bg-white rounded-lg shadow-lg h-">
+    <div className="py-4">
+      <div className="overflow-hidden bg-white rounded-lg shadow-lg">
         <div className="flex p-2 space-x-2 bg-gray-100">
           <FaLaptopCode className="w-auto h-5 text-gray-500" />
           <FaAward className="w-auto h-5 text-gray-500" />
           <FaGlobe className="w-auto h-5 text-gray-500" />
         </div>
 
-        {/* Course Title className="flex items-center p-4 space-x-3 text-white bg-custom-gradient rounded-t-2xl  h-44"*/}
-        {mydata.map((mydata, index) => (
-          <div>
-            <div className="h-60 ">
-              <img
-                src={icon1}
-                className=" bg-contain h-72 w-full rounded-t-2xl "
-              />
-            </div>
-            <h1 className=" relative font-bold  text-red-600 px-4 mb-5">
-              {mydata.title}
-            </h1>
-          </div>
-        ))}
+        {/* Course Title */}
+        <div className="h-60">
+          <img
+            src={course.image}
+            alt={course.title}
+            className="w-full bg-contain h-72 rounded-t-2xl"
+          />
+        </div>
+        <h1 className="relative px-4 mb-5 font-bold text-white">
+          {course.title}
+        </h1>
 
-        {/* <span className="text-lg font-semibold">
-            Master Data Structures and Algorithms using C++
-          </span> */}
         {/* Course Details */}
         <div className="p-4">
-          <h5 className="mb-2 text-lg font-semibold">This course includes</h5>
+          <h5 className="mb-2 text-lg font-semibold">This course includes:</h5>
           <ul className="space-y-2">
             <li className="flex items-center text-sm">
               <FaAward className="mr-2 text-yellow-500" />
-              350+ problems & 6 projects
+              {course.features.feature1}
             </li>
             <li className="flex items-center text-sm">
               <FaGlobe className="mr-2 text-green-500" />
-              Foundation, Basics, and Advanced modules
+              {course.features.feature2}
             </li>
             <li className="flex items-center text-sm">
               <BsFillPersonFill className="mr-2 text-blue-500" />
-              Progress tracking and feedback
+              {course.features.feature3}
             </li>
             <li className="flex items-center text-sm">
               <FaGraduationCap className="mr-2 text-purple-500" />
-              Certificate of Excellence/Completion
-            </li>
-            <li className="flex items-center text-sm">
-              <FaGlobe className="mr-2 text-red-500" />
-              Placement assistance
-            </li>
-            <li className="flex items-center text-sm">
-              <FaAward className="mr-2 text-pink-500" />
-              Doubt support
-            </li>
-            <li className="flex items-center text-sm">
-              <FaLaptopCode className="mr-2 text-indigo-500" />
-              Curriculum designed for beginners, No coding experience required
+              {course.features.feature4}
             </li>
           </ul>
         </div>
@@ -81,14 +50,14 @@ const CourseCard = () => {
         {/* Course Button */}
         <div className="flex justify-between p-4">
           <a
-            href="data-structures-and-algorithms-using-c-plus-plus.html"
-            className="px-2 py-2 text-center text-white bg-primary rounded-lg flex-shrink-0"
+            href={course.link}
+            className="flex-shrink-0 px-2 py-2 text-center text-white rounded-lg bg-primary"
           >
-            Show Course details
+             Start Now
           </a>
           <div className="ml-4">
-            <button className="px-4 py-2 bg-gray-200 rounded text-xl">
-              ₹. 4000
+            <button className="px-4 py-2 text-xl bg-gray-200 rounded">
+              {course.price}
             </button>
           </div>
         </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,76 +4,122 @@ import aboutImgBanner from "../assets/images/about-banner.jpg";
 import imgs from "../assets/images/join1.png";
 import { FaBookDead } from "react-icons/fa";
 import { AiOutlineCheck } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 export const About = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <section className='py-4 about'>
-        <div className='container'>
-          <div className='py-12 text-center heading'>
-            <h1 className='text-3xl font-semibold text-black'>Why An Scholercity Out Of The Ordinary</h1>
-            <span className='block mt-2 text-sm'>You don't have to struggle alone; you've got our assistance and help.</span>
+      <section className="py-4 about">
+        <div className="container">
+          <div className="py-12 text-center heading">
+            <h1 className="text-3xl font-semibold text-black">
+              Why An Scholercity Out Of The Ordinary
+            </h1>
+            <span className="block mt-2 text-sm">
+              You don't have to struggle alone; you've got our assistance and
+              help.
+            </span>
           </div>
-          <div className='grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4'>
-            <AboutCard color='bg-[#000]' icon={<FaBookDead size={50} />} title='4,000 Online Courses' desc="You don't have to struggle alone, you've " />
-            <AboutCard color='bg-[#000]' icon={<FaBookDead size={50} />} title='Expert Instructors' desc="Learn from industry leaders." />
-            <AboutCard color='bg-[#000]' icon={<FaBookDead size={50} />} title='Flexible Learning' desc="Study at your own pace." />
-            <AboutCard color='bg-[#000]' icon={<FaBookDead size={50} />} title='Certification' desc="Earn recognized certificates." />
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
+            <AboutCard
+              color="bg-[#000]"
+              icon={<FaBookDead size={50} />}
+              title="4,000 Online Courses"
+              desc="You don't have to struggle alone, you've "
+            />
+            <AboutCard
+              color="bg-[#000]"
+              icon={<FaBookDead size={50} />}
+              title="Expert Instructors"
+              desc="Learn from industry leaders."
+            />
+            <AboutCard
+              color="bg-[#000]"
+              icon={<FaBookDead size={50} />}
+              title="Flexible Learning"
+              desc="Study at your own pace."
+            />
+            <AboutCard
+              color="bg-[#000]"
+              icon={<FaBookDead size={50} />}
+              title="Certification"
+              desc="Earn recognized certificates."
+            />
           </div>
         </div>
       </section>
       <AboutContent />
     </>
   );
-}
+};
 
 export const AboutCard = (props) => {
   return (
-    <div className={`box shadow-md p-5 py-8 rounded-md text-white ${props.color} cursor-pointer transition ease-in-out delay-150 hover:-translate-y-4 duration-300`}>
-      <div className='text-white icon '>{props.icon}</div>
-      <div className='mt-5 text'>
-        <h4 className='my-3 text-lg font-semibold'>{props.title}</h4>
-        <p className='text-sm'>{props.desc}</p>
+    <div
+      className={`box shadow-md p-5 py-8 rounded-md text-white ${props.color} cursor-pointer transition ease-in-out delay-150 hover:-translate-y-4 duration-300`}
+    >
+      <div className="text-white icon ">{props.icon}</div>
+      <div className="mt-5 text">
+        <h4 className="my-3 text-lg font-semibold">{props.title}</h4>
+        <p className="text-sm">{props.desc}</p>
       </div>
     </div>
   );
-}
+};
 
 export const AboutContent = () => {
   return (
-    <section className='mb-16'>
-      <div className='container flex flex-col md:flex-row md:gap-8'>
-        <div className='relative w-full left md:w-1/3'>
-          <img src={aboutImg} alt='About' className='rounded-xl' />
-          <img src={aboutImgBanner} alt='About Banner' className='absolute h-56 rounded-xl -bottom-14 -left-24 md:left-20 md:-bottom-20' />
-          <div className='flex items-center mt-3 ml-0 img-group'>
-            <img src={imgs} alt='Join Us' className='w-16 h-16' />
-            <span className='text-[14px] ml-2'>
-              Join over <span className='text-sm text-black'>4,000+</span> students
+    <section className="mb-16">
+      <div className="container flex flex-col md:flex-row md:gap-8">
+        <div className="relative w-full left md:w-1/3">
+          <img src={aboutImg} alt="About" className="rounded-xl" />
+          <img
+            src={aboutImgBanner}
+            alt="About Banner"
+            className="absolute h-56 rounded-xl -bottom-14 -left-24 md:left-20 md:-bottom-20"
+          />
+          <div className="flex items-center mt-3 ml-0 img-group">
+            <img src={imgs} alt="Join Us" className="w-16 h-16" />
+            <span className="text-[14px] ml-2">
+              Join over <span className="text-sm text-black">4,000+</span>{" "}
+              students
             </span>
           </div>
         </div>
-        <div className='w-full right md:w-2/3 md:mt-16'>
-          <div className='heading'>
-            <h1 className='text-3xl font-semibold text-black'>Achieve Your Goals With Educal</h1>
-            <span className='block mt-2 text-sm leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam officia, reiciendis sapiente adipisci nulla non consequuntur eos repellendus laborum veritatis obcaecati neque est id porro voluptatum.</span>
-            <ul className='my-5'>
-              <li className='flex items-center gap-5 text-sm'>
-                <AiOutlineCheck className='text-green-500' /> Upskill your organization.
+        <div className="w-full right md:w-2/3 md:mt-16">
+          <div className="heading">
+            <h1 className="text-3xl font-semibold text-black">
+              Accelerate Your IT Career with Expert Training
+            </h1>
+            <span className="block mt-2 text-sm leading-6">
+              At Difmo Technologies, we offer industry-focused training
+              programs designed to equip IT and Computer Science students with
+              the skills they need to succeed in today’s fast-paced tech world.
+              From software development to data science and cybersecurity, our
+              hands-on courses provide practical knowledge that will set you
+              apart in the competitive job market.
+            </span>
+            <ul className="my-5">
+              <li className="flex items-center gap-5 text-sm">
+                <AiOutlineCheck className="text-green-500" /> Upskill your
+                organization.
               </li>
-              <li className='flex items-center gap-5 my-2 text-sm'>
-                <AiOutlineCheck className='text-green-500' />
+              <li className="flex items-center gap-5 my-2 text-sm">
+                <AiOutlineCheck className="text-green-500" />
                 Access more than 100K online courses.
               </li>
-              <li className='flex items-center gap-5 text-sm'>
-                <AiOutlineCheck className='text-green-500' />
+              <li className="flex items-center gap-5 text-sm">
+                <AiOutlineCheck className="text-green-500" />
                 Learn the latest skills.
               </li>
             </ul>
-            <button className='px-5 py-2 text-sm border border-gray-300 rounded-md'>Apply Now</button>
+            <button onClick={()=>navigate("/internshipform")} className="px-5 py-2 text-sm border border-gray-300 rounded-md">
+              Apply Now
+            </button>
           </div>
         </div>
       </div>
     </section>
   );
-}
+};
