c1b856dcb0b0edb6c7152b1bba4b1064df6b149f
difmo <difmotech@gmail.com>
2024-11-17 17:23:16 +0530

    save

--- src/components/Events/EventModel.jsx
+++ src/components/Events/EventModel.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const EventModel = ({onClick}) => {
   return (
-    <div className="fixed w-full bg-red-500">
+    <div className="fixed w-full ">
       <div className="w-full overflow-hidden">
         <div className="flex animate-marquee">
-          <div className="text-xl text-white whitespace-nowrap">
-          <span className='p-1 bg-red-700 border rounded-full cursor-pointer' onClick={onClick}>Register now</span> Don't miss out on the chance to register for our exclusive Demo Class, happening on 20th November 2024 To 22th November 2024.   <span className='p-1 bg-red-700 border rounded-full cursor-pointer' onClick={onClick}>Register now</span> This is your opportunity to get a hands-on introduction to our expert-led training sessions. Register now to secure your spot for the Demo Class scheduled on 20/11/2024, and take the first step towards mastering the skills that will propel your career. Don't wait—sign up today for the upcoming Demo Class on 20/11/2024 and start your journey towards success!<span className='p-1 bg-red-700 border rounded-full cursor-pointer' onClick={onClick}>Register now</span>
+          <div className="text-xl text-black whitespace-nowrap">
+        Don't miss out on the chance to register for our exclusive Demo Class, happening on 20th November 2024 To 22th November 2024.   <span className='p-1 text-white border rounded-full cursor-pointer bg-primary' onClick={onClick}>Register now</span> This is your opportunity to get a hands-on introduction to our expert-led training sessions. <span className='p-1 text-white border rounded-full cursor-pointer bg-primary' onClick={onClick}>Register now</span>
           </div>
         
         </div>
@@ -25,7 +25,7 @@ const EventModel = ({onClick}) => {
 
         .animate-marquee {
           display: flex;
-          animation: marquee 60s linear infinite; /* Adjust the speed here (60s) */
+          animation: marquee 50s linear infinite; /* Adjust the speed here (60s) */
         }
 
         /* Ensure the parent container has overflow hidden */
