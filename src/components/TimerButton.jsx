0198bc54ee6a9d07b3e4d18075d991a5f3a13442
dineshappdeveloper <dineshkumartlbupi@gmail.com>
2025-01-09 16:01:38 +0530

    save

--- src/components/TimerButton.jsx
+++ src/components/TimerButton.jsx
@@ -20,7 +20,7 @@ const TimerButton = () => {
         const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-// sdfdfs
+// sdfdfsdefrds
         return {
             hours,
             minutes,
