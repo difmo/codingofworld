cd590e440352abf59e9241ab8d5c5595779e5e08
7844Pritam <pritamsharma7844@gmail.com>
2025-05-29 07:31:24 +0530

    save

--- src/Utils/Constants.jsx
+++ src/Utils/Constants.jsx
@@ -1,33 +1,29 @@
-// utils/FormDataHelper.js (or any file you prefer)
+// utils/Constants.js
 
-class Constants {
-  static getInternshipPrograms() {
-    return [
-      "Mobile App Development",
-      "Web App Development",
-      "Apprenticeship Training",
-      "Summer Training",
-      "Winter Training",
-      "Digital Marketing",
-      "ASP.Net Development",
-      "PHP Development",
-      "Python Development",
-      "Java Development",
-      "Frontend Development",
-      "WordPress Development",
-      "Search Engine Optimization",
-      "C# Programming",
-      "C++ Programming",
-      "Java Programming",
-      "PHP Programming",
-      "Python Programming",
-      "Graphics Design",
-    ];
-  }
+export const getInternshipPrograms = () => [
+  "Mobile App Development",
+  "Web App Development",
+  "Apprenticeship Training",
+  "Summer Training",
+  "Winter Training",
+  "Digital Marketing",
+  "ASP.Net Development",
+  "PHP Development",
+  "Python Development",
+  "Java Development",
+  "Frontend Development",
+  "WordPress Development",
+  "Search Engine Optimization",
+  "C# Programming",
+  "C++ Programming",
+  "Java Programming",
+  "PHP Programming",
+  "Python Programming",
+  "Graphics Design",
+];
 
-  static getFormSteps() {
-    return ["Personal Info", "Academic Info", "Upload Resume"];
-  }
-}
-
-export default Constants;
+export const getFormSteps = () => [
+  "Personal Info",
+  "Academic Info",
+  "Upload Resume",
+];
